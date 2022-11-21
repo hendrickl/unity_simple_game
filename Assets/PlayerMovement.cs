8abc7fab487c2cf26f53487759204cd303c0b4ff
hendrickl <65900996+hendrickl@users.noreply.github.com>
2022-11-22 00:21:32 +0100

    Refactoring movement functionality

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,39 +5,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Time.deltaTime renvoie le temps en seconde écoulé depuis la derniere frame
+    // velocity : combine la notion de vitesse et celle de direction
+
+    Rigidbody rigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody>();
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        //Get the value of the Horizontal input axis.
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //Get the value of the Vertical input axis.
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rigidBody.velocity = new Vector3(horizontalInput * 5f, rigidBody.velocity.y, verticalInput * 5f);
+
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 5, 0);
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, 5f, rigidBody.velocity.z);
         }
 
-        if (Input.GetKey("up"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
-        }
-
-        if (Input.GetKey("down"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -5);
-        }
-
-        if (Input.GetKey("right"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(5, 0, 0);
-        }
-
-        if (Input.GetKey("left"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(-5, 0, 0);
-        }
     }
 }
